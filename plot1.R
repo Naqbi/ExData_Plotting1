d1370a9df0f8cccaa6d2d62d18dd7037daf7f5bb
Ahmed Al Naqbi <aqusoom@hotmail.com>
2021-08-30 09:10:13 +0400

    Fix error in hist function.

--- plot1.R
+++ plot1.R
@@ -5,5 +5,5 @@ subset_data <- subset(data, Date %in% c("1/2/2007","2/2/2007"))
 global_active_power <- as.numeric(subset_data$Global_active_power)
 png("~/ExData_Plotting1/figure/plot1.png", width = 480, height = 480)
 
-hist(aglobal_active_power, col="red", main="Global Active Power", xlab="Global Active Power (kilowatts)")
+hist(global_active_power, col="red", main="Global Active Power", xlab="Global Active Power (kilowatts)")
 dev.off()
